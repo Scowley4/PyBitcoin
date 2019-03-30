15ff7a831266bd4ffc74795cab2a0055b68051db
Stephen Cowley <StephenCowley44@gmail.com>
2019-03-30 16:00:04 -0600

    Added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='PyBitcoin',
+    description='Implementation of Bitcoin protocol in Python',
+    authod='Stephen Cowley',
+    url='https://github.com/scowley4/PyBitcoin',
+    packages=['PyBitcoin']
+)
