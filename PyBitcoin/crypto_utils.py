8ec286779980b58aca72e5bd4c7c01564cb3b2af
Stephen Cowley <StephenCowley44@gmail.com>
2019-03-31 10:11:39 -0600

    Added merkle hash function

--- PyBitcoin/crypto_utils.py
+++ PyBitcoin/crypto_utils.py
@@ -1,4 +1,20 @@
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
 from Crypto.Hash import SHA256
 
-def double_SHA256(data):
-    return (SHA256.new((SHA256.new(data)).digest())).digest()
+def doubleSHA256(bytedata):
+    """Returns the byte rep of a double SHA256 hash of the input bytedata."""
+    return (SHA256.new((SHA256.new(bytedata)).digest())).digest()
+
+# http://www.righto.com/2014/02/bitcoin-mining-hard-way-algorithms.html
+def concathex_doubleSHA256(hex1, hex2):
+    """Returns the doubleSHA256 concatination of two hexes.
+
+    This function is used in the merkletree for bitcoin. Note that this handles
+    the difference in endianness as well.
+    """
+    # Reverse for big-endian/little-endian conversion
+    hash1 = bytes.fromhex(hash1)[::-1]
+    hash2 = bytes.fromhex(hash2)[::-1]
+    return double_SHA256(hash1+hash2)[::-1].hex()
