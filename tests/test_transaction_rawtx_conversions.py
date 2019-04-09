efd1e7f39024e4d9bab8b0178e54c91691688443
Stephen Cowley <stephencowley44@gmail.com>
2019-04-09 13:57:29 -0600

    Restructure

--- tests/test_transaction_rawtx_conversions.py
+++ tests/test_transaction_rawtx_conversions.py
@@ -1,4 +1,4 @@
-from PyBitcoin import transaction_hash as th
+from PyBitcoin.transaction import tx_to_raw
 import json
 import os
 import testing_utils # utils for testing
@@ -9,13 +9,13 @@ TXSDIR = os.path.join(DATADIR, 'txs')
 def test_genesis_tx_to_raw():
     with open(os.path.join(DATADIR, 'genesis_tx.txt')) as infile:
         tx = json.load(infile)
-        h = th.tx_to_raw(tx)
+        h = tx_to_raw(tx)
         assert h == tx['rawtx']
 
 def test_tx_to_raw():
     """Test that all txs are built to their correct rawtx."""
     for tx, filename in testing_utils.gen_alltxs():
-        rawtx = th.tx_to_raw(tx)
+        rawtx = tx_to_raw(tx)
         assert rawtx == tx['rawtx'], (f'{filename} error in tx_to_raw')
 
 def test_tx_to_raw_each():
@@ -24,7 +24,7 @@ def test_tx_to_raw_each():
     error = []
     for tx, filename in testing_utils.gen_alltxs():
         try:
-            rawtx = th.tx_to_raw(tx)
+            rawtx = tx_to_raw(tx)
             if rawtx != tx['rawtx']:
                 wrong.append(filename)
         except:
